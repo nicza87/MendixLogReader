fbdc5c3f3dbb1f2f0b3fe98007c630eccafc0a29
nicza87 <97557328+nicza87@users.noreply.github.com>
2022-12-11 08:31:39 +0200

    check in outstanding work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import glob
-import pandas
+import pandas as pd
 import re
 
 def get_glob_list():
@@ -7,32 +7,52 @@ def get_glob_list():
     return glob_list
 
 def read_files(glob_list):
+    df = pd.DataFrame(columns = ['date', 'users'])
     for file in glob_list:
         with open(file) as f:
             for line in f.readlines():
-                parse_line(line)
-
+                parsed = parse_line(line)
+                if parsed:
+                    df.loc[len(df.index)] = [parsed[0], parsed[1]] 
+    #convert the 'Date' column to datetime format
+    df['date']= pd.to_datetime(df['date'])
+    df.sort_values(by='date')
+    return df
+    #print(df.to_string(index=False))
 
 #TODO: regex
 def parse_line(line):
-    # try:
         x = re.search('\(Number\sof\sconcurrent\ssessions:\s',line)
         if x:
-            #try:
             d_time =  line[:23]
             y = re.search("\ssessions:\s", line)
             if y:
                 c_users = line[y.end():-3]
-                print (d_time, c_users)
+                return (d_time, c_users)
             else:
                  print('ISSUE '+ line )
-            
-            #print()
-    #except:
-    #    print('no')
-    #if x:
-    #    print(line)
+        else:
+            return False
 
 if __name__ == '__main__':
-    print('testing')
-    read_files(get_glob_list())
+    
+    '''
+    print('Parsing data from files...')
+    df = read_files(get_glob_list())
+
+    print('Saving data to CSV...')
+    df.to_csv('concurrent_users.csv', index = False)
+    ''' 
+
+    print('Read parsed data from CSV...')
+    df = pd.read_csv('concurrent_users.csv')
+
+    df['date']= pd.to_datetime(df['date'])
+    print(df.info())
+    print(df)
+    date_max = df['date'].max()
+    date_min = df['date'].min()
+    print (date_min)
+    print (date_max)
+
+
